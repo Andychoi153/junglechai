87a429fb4bfd3f78f0bdc45e1f1a488cbdf953b4
Joseph <joseph@igaworks.com>
2020-02-01 18:20:43 +0900

    url 수정 및 대기 시간 수정

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 import datetime
-
+import time
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from queues import queues, matches
@@ -9,7 +9,6 @@ from .forms import ProfileForm
 
 
 def index(request):
-
     # POST 요청이면 폼 데이터를 처리한다
     if request.method == 'POST':
 
@@ -18,20 +17,34 @@ def index(request):
         # 응답 받으면 채워 넣고 response
 
         profile = ProfileForm(request.POST)
-
         if profile.is_valid():
+            match_flag = 0
             profile.profile_id = profile.cleaned_data['profile_id']
             profile.tear = profile.cleaned_data['tear']
             tear = profile.tear
             profile_id = profile.profile_id
             queues[(int(tear)-1)].put(profile.profile_id)
-
+            start = time.time()
             while True:
                 values = matches.get(profile.profile_id)
                 if values:
+                    matches.update({profile.profile_id: None})
+                    match_flag = 1
+                    break
+                if time.time() - start > 180:
+                    profile_id = queues[(int(tear) - 1)].get()
+                    if profile_id != profile.profile_id:
+                        queues[(int(tear) - 1)].put(profile.profile_id)
                     break
 
-            return HttpResponseRedirect(f'/chat/{values}?profile_id={profile_id}')
+            if match_flag == 1:
+                return HttpResponseRedirect(f'/chat/{values}?profile_id={profile_id}')
+            else:
+                context = {
+                    'form': profile,
+                }
+
+                return render(request, 'main/index.html', context)
 
     else:
         profile = ProfileForm()
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    url(r'^main/', include('main.urls')),
+    url(r'^', include('main.urls')),
     url(r'^chat/', include('chat.urls')),
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
 ]
