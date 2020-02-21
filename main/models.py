e1acb7a41ed0d0af5023c73cbb974ac23e67ef95
Joseph <joseph@igaworks.com>
2020-02-21 20:42:12 +0900

    일단 랜덤

--- main/models.py
+++ main/models.py
@@ -3,14 +3,6 @@ from django.db import models
 
 
 class Profile(models.Model):
-    TEAR_CHOICES = ((1, '아이언'),
-                    (2, '브론즈'),
-                    (3, '실버'),
-                    (4, '골드'),
-                    (5, '플래티넘'),
-                    (6, '다이아'),
-                    (7, '마스터'),
-                    (8, '그랜드 마스터'),
-                    (9, '챌린저'))
+    TEAR_CHOICES = ((1, '일반'))
     tear = models.IntegerField(choices=TEAR_CHOICES)
     lol_id = models.CharField(max_length=30)
